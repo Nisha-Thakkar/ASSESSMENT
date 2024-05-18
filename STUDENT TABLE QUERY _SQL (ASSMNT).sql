305f78e27cdcff3c6762c2723bcebff7e9f8649b
Nisha-Thakkar <nishathakkar200119@gmail.com>
2024-05-18 12:59:47 +0530

    Add files via upload

---
+++ STUDENT TABLE QUERY _SQL (ASSMNT).sql
@@ -0,0 +1,20 @@
+/*1.To display all the records form STUDENT table. SELECT * FROM student ;*/
+ SELECT * FROM STUDENT;
+ 
+ /*2.To display any name and date of birth from the table STUDENT. SELECT StdName, DOB
+FROM student ;*/
+SELECT STD_NAME, DOB FROM STUDENT;
+
+/*3.To display all students record where percentage is greater of equal to 80 FROM student table.
+SELECT * FROM student WHERE percentage >= 80;*/
+SELECT * FROM STUDENT WHERE PERCENTAGE >= 80;
+
+/*4.To display student name, stream and percentage where percentage of student is more than 80
+SELECT StdName, Stream, Percentage WHERE percentage > 80;*/
+SELECT STD_NAME, STREAM, PERCENTAGE
+FROM STUDENT WHERE PERCENTAGE > 80;
+
+/*5.To display all records of science students whose percentage is more than 75 form student table.
+SELECT * FORM student WHERE stream = ‘Science’ AND percentage > 75;*/
+SELECT * FROM STUDENT WHERE STREAM = "SCIENCE" AND PERCENTAGE > 75;
+  
